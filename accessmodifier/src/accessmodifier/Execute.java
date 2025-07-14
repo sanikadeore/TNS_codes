611ef6bd8cc205a8e706e51ee4637b7ff96d67ac
sanikadeore <sanikadeore2005@gmail.com>
2025-07-14 17:00:57 +0530

    Access_Modifiers

---
+++ accessmodifier/src/accessmodifier/Execute.java
@@ -0,0 +1,15 @@
+package accessmodifier;
+
+public class Execute {
+
+	public static void main(String[] args) {
+		
+		AccessModifier b = new AccessModifier();
+		
+		b.methodDefault();
+		b.methodProtected();
+		b.methodPublic();
+
+	}
+
+}
