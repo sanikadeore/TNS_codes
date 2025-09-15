5932760f1f5104669df253e0e5625f2d35c515c6
sanikadeore <sanikadeore2005@gmail.com>
2025-09-15 16:06:10 +0530

    Collections

---
+++ Collection/src/set/SetDemo.java
@@ -0,0 +1,21 @@
+package set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetDemo {
+
+	public static void main(String[] args) {
+
+          Set<Integer> set = new HashSet<Integer>();
+          set.add(20);
+          set.add(10);
+          set.add(30);
+          set.add(48);
+          set.add(20);
+          System.out.println(set);
+          
+          System.out.println(set.isEmpty());
+	}
+
+}
