8ca35ff055207d08fe964eb1d6162395f0e37815
sanikadeore <sanikadeore2005@gmail.com>
2025-09-15 16:12:03 +0530

    MultiThreading

---
+++ MultiThreading/src/multiT/ChildThread.java
@@ -0,0 +1,29 @@
+package multiT;
+
+//Thread class
+public class ChildThread extends Thread {
+   private int n;
+   private String msg;
+   
+	public ChildThread(int n, String msg) {
+		this.n = n;
+		this.msg = msg;
+	}
+	
+	@Override
+	public void run()
+	{
+		for(int i=1; i<=n; i++)
+		{
+//			try {
+//				Thread.sleep(1000);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+			System.out.println(msg+i);
+		}
+	}
+   
+   
+}
